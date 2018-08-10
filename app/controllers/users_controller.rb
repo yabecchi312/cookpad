ae80850fe53fa18c62ccbc46cda30e7581d2333b
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-08-10 19:45:50 +0900

    alter follower function
    ・userコントローラーの空行修正
    ・userコントローラーの繰り返されている記述はbefore_actionで定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user
   def show
-    @user = User.find(params[:id])
   end
+
   def follow
-    @user = User.find(params[:id])
-    #ログイン中のユーザーで対象のユーザー(@user)をフォローする
     current_user.follow(@user)
     respond_to do |format|
       format.html { redirect_to @user }
@@ -13,12 +12,16 @@ class UsersController < ApplicationController
   end
 
   def unfollow
-    @user = User.find(params[:id])
-    #ログイン中のユーザーで対象のユーザー(@user)をフォロー解除する
     current_user.stop_following(@user)
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
