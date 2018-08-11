da8bafb004414490141683e913e4b53f176ab8d6
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-08-11 19:17:58 +0900

    s3 setting2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'cookpad'
+  config.fog_directory  = 'cookpad-yabecchi312'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cookpad-yabecchi312'
 end
