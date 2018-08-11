bff0cc590130ab75c5de60d6b52b9348cf9580b3
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-11 16:00:35 +0900

    gather-routing
    ルーティングをまとめました

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
-  resources :recipes, only: [:index, :new]
+  resources :recipes, only: [:index, :new,:create]
   resources :users, only: [:show] do
     member do
       get 'follow'
       get 'unfollow'
     end
   end
-  resources :recipes, only: [:index, :new, :create]
-  resources :users, only: [:show]
 end
