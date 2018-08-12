9072b573a6a87d72205482e12fadaae693c1bd00
gumpen <shuhei1993727@gmail.com>
2018-08-12 16:52:57 +0900

    Show recipe contents

--- app/controllers/myfolders_controller.rb
+++ app/controllers/myfolders_controller.rb
@@ -1,5 +1,9 @@
 class MyfoldersController < ApplicationController
   def index
     @user = User.find(current_user.id)
+    @recipes = []
+    @user.myfolders.each do |myfolder|
+      @recipes.push(myfolder.recipe)
+    end
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :flows
+  has_many :myfolders
   accepts_nested_attributes_for :ingredients
   accepts_nested_attributes_for :flows
   mount_uploader :image, ImageUploader
