e06ad14a367c046d8e9169c35766feb06ba367ca
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-08-13 20:51:38 +0900

    user edit view
    userのeditページのビューを作成

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -17,7 +17,6 @@ $(document).on('turbolinks:load', function() {
   $('#user_menu_config').on("click", function() {
     $('body').append('<div id="fade-bg"></div>')
     $('.config_dropdown_menu,#fade-bg').fadeIn();
-    console.log(this);
     $('#fade-bg').click(function() {
     $('.config_dropdown_menu,#fade-bg').fadeOut(function(){
       $('#fade-bg').remove()
