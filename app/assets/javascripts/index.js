f890816bddb6aecec872ee751beedb09ecf6824d
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-08-13 21:10:29 +0900

    alter index.js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -13,21 +13,3 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
-
-  $('#user_menu_config').on("click", function() {
-    $('body').append('<div id="fade-bg"></div>')
-    $('.config_dropdown_menu,#fade-bg').fadeIn();
-    $('#fade-bg').click(function() {
-    $('.config_dropdown_menu,#fade-bg').fadeOut(function(){
-      $('#fade-bg').remove()
-    });
-  });
-});
-
-  // $('#user_menu_config').on("click", function() {
-  //   $('.config_dropdown_menu').fadeIn();
-  //   $('body').click(function() {
-  //     $('.config_dropdown_menu').fadeOut();
-  //         console.log(this);
-  //   });
-  // });
