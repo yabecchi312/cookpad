553fd7a9c2bb145de07cdc51726c6e2306a89a0b
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-08-15 19:47:18 +0900

    validate name maxlength10
    ・user-nameのmaxlengthを10文字でvalidateをかける。
    ・カウントをして、あと何文字という表示をする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, ImageUploader
+   validates :name, length: {maximum: 10}
 end
