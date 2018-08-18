9a2b00381e57279fea6c3cca80b105121c52d5ef
Maiko Naruse <maimai@Maiko-no-MacBook-Air.local>
2018-08-18 16:54:25 +0900

    only→exceptに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tops#index'
-  resources :recipes, only: [:index, :new,:create]
+  resources :recipes, except: [:edit, :update]
   resources :users, only: [:show, :edit, :update] do
     member do
       get 'follow'
       get 'unfollow'
     end
   end
-<<<<<<< HEAD
-  resources :recipes, except: [:show, :edit, :update]
-=======
-
-  resources :recipes, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :create]
   resources :users, only: [:show]
   resources :myfolders, only: [:index, :create, :destroy]
->>>>>>> origin/recipe-delete-function
   get '/recipes/list/:id', to: 'recipes#list'
 end
