8dea191ba6f91abe4a0a7c397b11a87d9e567222
shoji_hiroya <uncut8@shoujiyuuyanoMacBook-Pro.local>
2018-08-18 16:33:58 +0900

    Create routes-diaries

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,7 @@ Rails.application.routes.draw do
       get 'unfollow'
     end
   end
-  resources :recipes, only: [:index, :new, :create]
-  resources :users, only: [:show]
   resources :myfolders, only: [:index, :create, :destroy]
   get '/recipes/list/:id', to: 'recipes#list'
-  resources :diarys, only: [:index, :new, :create]
-  get 'diarys/new/:id', to: 'diarys#new'
+  resources :diaries, only: [:index, :new, :create]
 end
