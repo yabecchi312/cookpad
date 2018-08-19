658a59c978b54b74013f75e8891a1da4dda2a56e
Maiko Naruse <maimai@Maiko-no-MacBook-Air.local>
2018-08-19 19:01:08 +0900

    commentsテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180812070453) do
+ActiveRecord::Schema.define(version: 20180819095116) do
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",                  null: false
+    t.integer  "recipe_id",                null: false
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["recipe_id"], name: "index_comments_on_recipe_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "flows", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "recipe_id",                null: false
@@ -85,6 +95,8 @@ ActiveRecord::Schema.define(version: 20180812070453) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "comments", "recipes"
+  add_foreign_key "comments", "users"
   add_foreign_key "flows", "recipes"
   add_foreign_key "ingredients", "recipes"
   add_foreign_key "myfolders", "recipes"
