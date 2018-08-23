edc3a0f3dd669b036a5773133790c2d6bb1f4a0f
gumpen <shuhei1993727@gmail.com>
2018-08-23 17:26:48 +0900

    Add kondates/new view

--- app/controllers/kondates_controller.rb
+++ app/controllers/kondates_controller.rb
@@ -2,6 +2,10 @@ class KondatesController < ApplicationController
   def index
   end
 
+  def new
+    @kondate = Kondate.new
+  end
+
   def recent
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   resources :myfolders, only: [:index, :create, :destroy]
   get '/recipes/list/:id', to: 'recipes#list'
   resources :search, only: [:index]
-  resources :kondates, only: [:index]
+  resources :kondates, only: [:index,:new]
   get '/kondates/recent', to: 'kondates#recent'
 end
