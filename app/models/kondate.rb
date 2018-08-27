47a3185f81ccf30fd3b17ccd584d482cb509d15d
gumpen <shuhei1993727@gmail.com>
2018-08-27 17:29:47 +0900

    Add comment

--- app/models/kondate.rb
+++ app/models/kondate.rb
@@ -7,6 +7,7 @@ class Kondate < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
+  # 検索ワードでレシピと献立のタイトルを検索し、かかった献立idを配列で返す
   def self.select_target_kondate_id(keyword)
     recipe_ids = Recipe.select_target_recipe_id(keyword)
     kondate_ids = RecipeKondate.where("recipe_id IN (?)",recipe_ids).select(:kondate_id).map{|obj| obj[:kondate_id]}
