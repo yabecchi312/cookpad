b3211a60e095c408fffee8922a6d85d1f199c453
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-29 20:56:25 +0900

    change-recipe-main-image
    レシピのメイン画像の置き換えの記述をしました。

--- app/assets/javascripts/recipe_image.js
+++ app/assets/javascripts/recipe_image.js
@@ -1,8 +1,21 @@
 $(function(){
-  var html = `<div class="" `
+  // 新規,編集レシピメイン画像
+  $(".recipe-image").on("change", function(){
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
 
-  $(".recipe-photo").on("click", function(){
-    $(this).find("#recipe-main-photo").remove();
-    $("#recipe-main-photo").append(html);
+    reader.onload = function() {
+      $("#image_example").attr("src", reader.result)
+    };
   });
-})
+  // $(".step-image").on("change", function(){
+  //   var file = this.files[0];
+  //   var reader = new FileReader();
+  //   reader.readAsDataURL(file);
+
+  //   reader.onload = function() {
+  //     $("#step_example").attr("src", reader.result)
+  //   };
+  // })
+});
