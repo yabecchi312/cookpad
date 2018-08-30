d651825e7e10b609f32fdf33a82ab5587be9d4da
宮田昌平 <heyheyshohey0104@gmail.com>
2018-08-30 17:28:47 +0900

    html5-API
    画像の置き換えの処理をしました。html5のAPIを使っています。

--- app/assets/javascripts/recipe_image.js
+++ app/assets/javascripts/recipe_image.js
@@ -9,13 +9,14 @@ $(function(){
       $("#image_example").attr("src", reader.result)
     };
   });
-  // $(".step-image").on("change", function(){
-  //   var file = this.files[0];
-  //   var reader = new FileReader();
-  //   reader.readAsDataURL(file);
-
-  //   reader.onload = function() {
-  //     $("#step_example").attr("src", reader.result)
-  //   };
-  // })
+  // 新規料理手順の画像
+  $(document).on("change", ".step-image", function(){
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    var StepImage = $(this).prev('.step_example')
+    reader.onload = function() {
+      $(StepImage).attr("src", reader.result)
+    };
+  });
 });
