a9f49b4a2890e6b87673e61abb68e8b24673c8ec
shoji_hiroya <uncut8@shoujiyuuyanoMacBook-Pro.local>
2018-08-30 16:27:19 +0900

    edit_Diaries_controller

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,7 +1,7 @@
 class DiariesController < ApplicationController
   def index
-    @user = User.find(current_user.id)
-    @diaries = @user.diaries.order(id: :desc)
+    @user = User.find(current_user)
+    @diaries = current_user.diaries.order(id: :desc)
   end
 
   def new
@@ -21,7 +21,7 @@ class DiariesController < ApplicationController
   def destroy
     @diary = Diary.find(params[:id])
     @diary.destroy
-    redirect_to action: "index", id: current_user.id
+    redirect_to diaries_path
   end
 
   private
