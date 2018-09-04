a1df7771d18ea77c7769586875b8d1c4072934c6
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-09-04 19:53:25 +0900

    alter upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  process resize_to_fit: [200, 200]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
