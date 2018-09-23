88c14731b801aaa9d64eb4162a57f27aeafb1752
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-09-23 18:35:54 +0900

    validate test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,35 @@ describe User do
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
+
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とPasswordの入力が一致しません")
+    end
+
+    it "is invalid a name that has more than 11 characters" do
+      user = build(:user, name: "aaaaaaaaaaa")
+      user.valid?
+      expect(user.errors[:name][0]).to include("は10文字以内で入力してください")
+    end
+
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user, email: user.email)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("はすでに存在します")
+    end
+
+    it "is valid with a name that has less than 10 characters " do
+      user = build(:user, name: "aaaaaaaaaa")
+      expect(user).to be_valid
+    end
+
+    it "is invalid with a password that has less than 5 characters " do
+      user = build(:user, password: "00000", password_confirmation: "00000")
+      user.valid?
+      expect(user.errors[:password][0]).to include("は6文字以上で入力してください")
+    end
   end
 end
